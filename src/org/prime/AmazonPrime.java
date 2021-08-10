829b5c4c9ad408051082bf1a095c4d8c0faf8355
AravinthMaddy <aravinthmaddy5@gmail.com>
2021-08-10 19:41:27 +0530

    B Merge

--- src/org/prime/AmazonPrime.java
+++ src/org/prime/AmazonPrime.java
@@ -7,6 +7,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class AmazonPrime {
+	
+	
+	private void cusId() {
+	System.out.println("ud is 23");
+
+	}
+	private void cusName() {
+	System.out.println("name is ara");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 public static void main(String[] args) throws InterruptedException {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\aravi\\eclipse-workspace\\Amazon\\driver\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
